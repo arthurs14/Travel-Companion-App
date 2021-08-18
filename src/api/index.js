4acc5f11ff7ff0338da27767d47e10fa03b8575a
arthurs14 <arthurcs426@gmail.com>
2021-08-18 14:00:55 -0700

    updating the coordinates to be more exact

--- src/api/index.js
+++ src/api/index.js
@@ -1,23 +1,21 @@
 import axios from 'axios';
 import { travelURL, travelKeyAPI, travelHost } from '../keys/keys';
 
-var options = {
-  params: {
-    bl_latitude: '11.847676',
-    tr_latitude: '12.838442',
-    bl_longitude: '109.095887',
-    tr_longitude: '109.149359',
-  },
-  headers: {
-    'x-rapidapi-key': travelKeyAPI,
-    'x-rapidapi-host': travelHost
-  }
-};
-
-export const getPlacesData = async () => {
+export const getPlacesData = async (sw, ne) => {
   try {
     // request
-    const { data: { data } } = await axios.get(travelURL, options);
+    const { data: { data } } = await axios.get(travelURL, {
+      params: {
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+      },
+      headers: {
+        'x-rapidapi-key': travelKeyAPI,
+        'x-rapidapi-host': travelHost
+      }
+    });
 
     return data;
   } catch (error) {
